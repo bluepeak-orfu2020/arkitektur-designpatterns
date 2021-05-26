0d0d3180b739014343fb6324ac352533ee3e50fb
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-05-26 13:31:00 +0200

    Örebro grupp 4 - applied command pattern

--- Örebro grupp 4/Arkitektur/Program.cs
+++ Örebro grupp 4/Arkitektur/Program.cs
@@ -12,28 +12,92 @@ namespace Arkitektur
         {
             Console.WriteLine("Vill du addera(+), subtrahera(-) eller dividera(/)?");
             string opt = Console.ReadLine();
+
+            MathCommmand command = null;
             if (opt == "+") {
                 Console.WriteLine("Skriv två tal");
                 var tal1 = Convert.ToInt32(Console.ReadLine());
                 var tal2 = Convert.ToInt32(Console.ReadLine());
-                var sum = tal1 + tal2;
-                Console.WriteLine($"Resultatet av adderingen blev: {sum}");
+                command = new AdditionCommand(tal1, tal2);
             } else if (opt == "-") {
                 Console.WriteLine("Skriv två tal");
                 var tal1 = Convert.ToInt32(Console.ReadLine());
                 var tal2 = Convert.ToInt32(Console.ReadLine());
-                var sum = tal1 - tal2;
-                Console.WriteLine($"Resultatet av subtraheringen blev: {sum}");
+                command = new SubtractionCommand(tal1, tal2);
             } else if (opt == "/") {
                 Console.WriteLine("Skriv två tal");
                 var tal1 = Convert.ToInt32(Console.ReadLine());
                 var tal2 = Convert.ToInt32(Console.ReadLine());
-                var sum = tal1 / tal2;
-                Console.WriteLine($"Resultatet av divideringen blev: {sum}");
+                command = new DivisionCommand(tal1, tal2);
             } else {
                 Console.WriteLine("Ogiltigt val");
             }
+
+            if (command != null)
+            {
+                command.Execute();
+            }
+
             Console.ReadLine();
+        }
+
+        interface MathCommmand
+        {
+            void Execute();
+        }
+
+        class AdditionCommand : MathCommmand
+        {
+            int x;
+            int y;
+
+            public AdditionCommand(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public void Execute()
+            {
+                var sum = x + y;
+                Console.WriteLine($"Resultatet av adderingen blev: {sum}");
+            }
+        }
+        
+        class SubtractionCommand : MathCommmand
+        {
+            int x;
+            int y;
+
+            public SubtractionCommand(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public void Execute()
+            {
+                var sum = x - y;
+                Console.WriteLine($"Resultatet av subtraktion blev: {sum}");
+            }
+        }
+        
+        class DivisionCommand : MathCommmand
+        {
+            int x;
+            int y;
+
+            public DivisionCommand(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public void Execute()
+            {
+                var sum = x / y;
+                Console.WriteLine($"Resultatet av divideringen blev: {sum}");
+            }
         }
     }
 }
