7c6131bf4c42b75799a5b3e21661f2205c3a858c
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-05-26 13:12:09 +0200

    Örebro grupp 3 - ursprunglig version

---
+++ Örebro grupp 3/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        } 
+    }
+
+    class ConcreteGrade10 : ITeacher
+    {
+        public void setGrade(IStudent s)
+        {
+            s.SetBadGrade(this);
+        }
+    }
+
+    class ConcreteGrade20 : ITeacher
+    {
+        public void setGrade(IStudent s)
+        {
+            s.SetGoodGrade(this);
+        }
+    }
+
+    interface ITeacher
+    {
+        void setGrade(IStudent s);
+
+        
+    }
+
+    interface IStudent
+    {
+        void SetBadGrade(ConcreteGrade10 c);
+
+        void SetGoodGrade(ConcreteGrade20 c);
+    }
+
+   class Elev : IStudent
+    {
+        public int Poäng { get; set; }
+
+        public void Plugga()
+        {
+            Poäng++;
+        }
+
+        public void SetBadGrade(ConcreteGrade10 c)
+        {
+            Poäng += 10;
+        }
+
+        public void SetGoodGrade(ConcreteGrade20 c)
+        {
+            Poäng += 20;
+        }
+    }
+
+}
---
+++ Örebro grupp 3/SolutionForVisitor/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Elev> elever = new List<Elev>
+            {
+                new Elev() { AmbitionsNivå = 20 },
+                new Elev() {AmbitionsNivå = 10},
+                new Elev() {AmbitionsNivå = 5}
+            };
+            
+            
+
+           foreach(var elev in elever)
+            {
+                elev.Plugga();
+            }
+
+       
+
+           
+            Console.WriteLine("Elev1 har nu uppnåt ambitionsnivån");
+
+            Console.WriteLine("Läraren kollar om eleven kommer att klara tentan");
+
+            foreach(var elev in elever)
+            {
+                Console.WriteLine(elev.klaratTentan());
+            }
+      
+            Console.ReadKey();
+        }
+    }
+
+    class Elev
+    {
+        public string Namn { get; set; }
+        public int KunskapsNivå { get; set; }
+
+        public int AmbitionsNivå { get; set; }
+
+        public void Plugga()
+        {
+            KunskapsNivå += 10;
+            Console.WriteLine("Eleven pluggar");
+        }
+
+        public void Spela()
+        {
+            KunskapsNivå--;
+        }
+        public bool klaratTentan()
+        {
+            if(KunskapsNivå > 30)
+            {
+                return true;
+            } else
+            {
+               return false;
+            }
+        }
+
+        public bool uppnåttAmbitionsNivå()
+        {
+            return KunskapsNivå >= AmbitionsNivå;
+        }
+       
+    }
+
+    
+}
